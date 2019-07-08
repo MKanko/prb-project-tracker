11aaece2f05f94349d2ed602113ece9766df5532
MKanko <mkanko66@gmail.com>
2019-07-07 21:09:02 -0700

    add get projects, get new actions to project controller and new project form to views/projects/new.erb

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,6 +7,25 @@ class ProjectsController < ApplicationController
         else 
             redirect to '/login'
         end 
-    end 
+    end
+    
+    get '/projects/new' do
+        if logged_in?
+            erb :'/projects/new'
+        else
+            redirect to '/login'
+        end
+    end
+ 
+    post '/projects' do
+        if params[:name] != ""
+            @project = Project.new(params)
+            @project.contractor = current_contractor
+            @project.save
+            redirect to "/projects/#{@project.id}"
+        else
+            redirect to '/projects/new'
+        end
+    end
 
 end 
