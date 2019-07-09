c1ebd3a9d03cdb7fe9a4baff8f00ed676cfb4ddc
MKanko <mkanko66@gmail.com>
2019-07-08 21:33:09 -0700

    add projects delete action to project controller, get sub action to sub controller, add index form to subs/idex.erb,add subs new action to subs controller, add new form to subs/new.erb, and post (create) action to subs controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -52,15 +52,27 @@ class ProjectsController < ApplicationController
     patch '/projects/:id' do
         @project = Project.find_by_id(params[:id])
         if params[:name] != "" && params[:project_scope] != "" && params[:total_cost] != ""
-           params[:sub_cost] != "" && params[:subcontractors] != "" && params[:status] != ""
-           params[:billing_status] !="" 
-           @project.update(name: params[:name], project_scope: params[:project_scope], 
-           total_cost: params[:total_cost], sub_cost: params[:sub_cost], 
-           subcontractors: params[:subcontractors], status: params[:status],
-           billing_status: params[:billing_status])
-           redirect to "/projects/#{@project.id}"  
+        params[:sub_cost] != "" && params[:subcontractors] != "" && params[:status] != ""
+        params[:billing_status] !="" 
+            @project.update(name: params[:name], project_scope: params[:project_scope], 
+            total_cost: params[:total_cost], sub_cost: params[:sub_cost], 
+            subcontractors: params[:subcontractors], status: params[:status],
+            billing_status: params[:billing_status])
+            redirect to "/projects/#{@project.id}"  
         else
             redirect to "/projects/#{@project.id}/edit"
+        end 
+    end 
+
+    delete '/projects/:id/delete' do 
+        if logged_in?
+            @project = Project.find_by_id(params[:id])
+            if @project && @project.contractor == current_contractor
+                @project.delete
+                redirect '/projects'  
+            end 
+        else 
+            redirect to '/login'
         end 
     end 
 
--- app/controllers/subcontractors_controller.rb
+++ app/controllers/subcontractors_controller.rb
@@ -1,8 +1,32 @@
 class SubcontractorsController < ApplicationController
 
-    get '/subcontractors' do 
-        @subcontractors = Subcontractor.all 
-        erb :'/subcontractors/index'
+    get '/subcontractors' do
+        if logged_in? 
+            @subcontractors = Subcontractor.all 
+            erb :'/subcontractors/index'
+        else 
+            redirect to '/login'
+        end 
     end 
+   
+    get '/subcontractors/new' do
+        if logged_in?
+            erb :'/subcontractors/new'
+        else
+            redirect to '/login'
+        end 
+    end 
+    
+    post '/subcontractors' do
+        if params[:name] != ""
+            @subcontractor = Subcontractor.new(params)
+            @subcontractor.save
+            redirect to "/subcontractors/#{@subcontractor.id}"
+        else
+            @error = "A subcontractor must have a name."
+            erb :'/subcontractors/new' 
+        end 
+    end 
+
 
 end 
